30ab31ca04720b688b5b9af39b620a902d35f3e3
Olivier Breuleux <breuleux@gmail.com>
2016-01-27 01:02:00 -0500

    changed brackets for EG, added some help about testing

--- generators/app/templates/js/index.js
+++ generators/app/templates/js/index.js
@@ -1,6 +1,8 @@
 
 function install(engine, options) {
     // Plugin code goes here.
+    // TIP: you can test your plugin with the command:
+    // quaint -p ./lib -e 'code that uses your custom rule or macro'
 }
 
 function main(quaintEngine, options) {
